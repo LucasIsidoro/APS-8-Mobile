5e987a001e170daf4b8b19ac06f0a401bfd697a2
André Macena <andrmacena_@outlook.com>
2019-10-29 21:17:49 -0300

    APS alterada

--- APS8Mobile/src/components/Map.js
+++ APS8Mobile/src/components/Map.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
-import { View,Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+
+import api from '../service/api'
 
 export default class Map extends Component {
-   render() {
-      return (
+  state = {
+    places: []
+  }
+
+  async componentDidMount() {
+    const response = await api.get('places')
+
+    this.setState({ places: response.data })
+  }
+  render() {
+    return (
       <View>
-         <Text>Map</Text>
+        <FlatList
+          data={this.state.places}
+          keyExtractor={place => place._id}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.localidade}</Text>
+              <Text>{item.latitude}</Text>
+              <Text>{item.longitude}</Text>
+            </View>
+          )}
+        />
       </View>
-      )
-   }
+    )
+  }
 }
--- APS8Mobile/src/components/Places.js
+++ APS8Mobile/src/components/Places.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
 
-import { View,Text } from 'react-native';
+import { View, TextInput } from 'react-native';
+
+import api from '../service/api'
 
 export default class Places extends Component {
+  
+
+//   handleSubmit = async () => {
+//     const data = new FormData()
+
+//       data.append('localidade', this.state.localidade)
+//       data.append('latitude', this.state.latitude)
+//       data.append('longitude', this.state.longitude)
+
+//       await api.post('places', data)
+
+//       //redirecionando o usuário para a rota 
+//       this.props.navigation.navigate('Map')
+//  }
+
   render() {
     return (
       <View>
-         <Text>Places</Text>
+        <TextInput
+               //style={styles.input}
+               autoCorrect={false}
+               autoCapitalize='none'
+               placeholder='Localidade'
+               placeholderTextColor='#999'
+               //value={this.state.localidade}
+               //onChangeText={localidade => this.setState({ localidade })}
+            />
+        <TextInput
+               //style={styles.input}
+               autoCorrect={false}
+               autoCapitalize='none'
+               placeholder='Latitude'
+               placeholderTextColor='#999'
+               //value={this.state.latitude}
+               //onChangeText={latitude => this.setState({ latitude })}
+            />
+        <TextInput
+               //style={styles.input}
+               autoCorrect={false}
+               autoCapitalize='none'
+               placeholder='Longitude'
+               placeholderTextColor='#999'
+               //value={this.state.longitude}
+               //onChangeText={longitude => this.setState({ longitude })}
+            />
+        
       </View>
-      )
+    )
   }
 }
