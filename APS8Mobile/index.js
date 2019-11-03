3447b26bf914ccb9a762d5a30d6ad9ca7769d79f
Lucas Rafael Isidoro <lucas.r.isidoro@gmail.com>
2019-11-03 16:00:27 -0300

    Gmaps funcional

--- APS8Mobile/index.js
+++ APS8Mobile/index.js
@@ -1,5 +1,18 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import 'react-native-gesture-handler';
+
+YellowBox.ignoreWarnings([
+    //'Warning: Async Storage has been extracted',
+    'Battery state',
+    'componentWillMount',
+    'componentWillUpdate',
+    'componentWillReceiveProps',
+    '[location] ERROR - 0',
+    'Warning: DatePickerAndroid', // will be fixed with https://github.com/mmazzarolo/react-native-modal-datetime-picker/pull/262
+    'RCTRootView cancelTouches', // https://github.com/kmagiera/react-native-gesture-handler/issues/746
+  ]);
 
 AppRegistry.registerComponent(appName, () => App);
+
--- APS8Mobile/src/components/Map.js
+++ APS8Mobile/src/components/Map.js
@@ -1,34 +1,131 @@
 import React, { Component } from 'react';
 
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Dimensions } from 'react-native';
 
-import api from '../service/api'
+import MapView, { Marker, AnimatedRegion, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import Geolocation from '@react-native-community/geolocation';
+
+import api from '../service/api';
+
+let { width, height } = Dimensions.get('window');
 
 export default class Map extends Component {
-  state = {
-    places: []
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      width: width,
+      marginBottom: 1,
+      region: null,
+      ready: false,
+      bottomMargin: 1,
+      places: []
+    };
+  }
+
+  async getCurrentPosition() {
+    try {
+      Geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+        const region = ({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: 0.00543,
+            longitudeDelta: 0.00534,
+          }
+        });
+        this.setState(region);
+        ;
+      }, (error) => {
+        //TODO: better design
+        switch (error.code) {
+          case 1:
+            if (Platform.OS === "ios") {
+              Alert.alert("", "Para habilitar permisão de acesso a Localizaçào entre em Ajustes - Privacidade - Localização");
+            }
+            else {
+              Alert.alert("", "Para habilitar permisão de acesso a Localizaçào entre em Ajustes - Apps - ShareTools - Localização");
+            }
+            break;
+          default:
+            Alert.alert("", "Erro ao identificar sua localização");
+        }
+      }, {
+        timeout: 20000,
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+      });
+    } catch (e) {
+      alert(e.message || "");
+    }
   }
 
   async componentDidMount() {
+    this.getCurrentPosition();
     const response = await api.get('places')
-
     this.setState({ places: response.data })
   }
+
+  // onMapReady = (e) => {
+  //   if (!this.state.ready) {
+  //     this.setState({ marginBottom: 1 });
+  //     this.setState({ ready: true });
+  //   }
+  //   this.setState({ marginBottom: 1 });
+  // };
+
   render() {
+    //const { region } = this.state;
+    //const { children, renderMarker, markers } = this.props;
+
     return (
-      <View>
-        <FlatList
-          data={this.state.places}
-          keyExtractor={place => place._id}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.localidade}</Text>
-              <Text>{item.latitude}</Text>
-              <Text>{item.longitude}</Text>
-            </View>
-          )}
-        />
+
+      <View style={styles.container}>
+
+        <MapView
+          mapPadding={{
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+          }}
+
+          provider={PROVIDER_GOOGLE}
+          onMapReady={this.onMapReady}
+          style={{
+            marginBottom: this.state.bottomMargin,             // using state in styling
+            ...StyleSheet.absoluteFillObject,
+          }}
+          initialRegion={this.state.region}
+          region={this.state.region}
+          onRegionChangeComplete={this.onRegionChange}
+          onMapReady={() => this.setState({ bottomMargin: 0 })}
+          followsUserLocation={true}
+          showsUserLocation
+          loadingEnabled={true}
+          scrollEnabled={true}
+          zoomEnabled={true}
+          showsCompass={true}
+          showsBuildings={false}
+          showsPointsOfInterest={false}
+          showsMyLocationButton={true}
+        //ref={map => { this.map = map }}
+        >
+        </MapView>
+
       </View>
-    )
+
+
+    );
   }
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+  },
+
+});
